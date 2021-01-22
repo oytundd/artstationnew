893d7a4a29ff480dfc28003a5be966f028932407
oytundd <oytundd@gmail.com>
2021-01-22 16:34:34 +0300

    Update newIndex.js

--- newIndex.js
+++ newIndex.js
@@ -2,7 +2,8 @@
 
 let userUrl = "https://www.artstation.com/users/lemonhunter/likes.json";
 let userName = "lemonhunter";
-
+let userJson;
+let randomArtwork;
 async function fetchJson(url) {
   const response = await fetch(url);
   if (response.ok) {
@@ -12,7 +13,7 @@ async function fetchJson(url) {
     throw new Error(message);
   }
 }
-function updateJson(passedJson) {
+function updateJson(passedJson, userUrl) {
   passedJson = fetchJson(userUrl).then((passedJson) => {
     chrome.storage.local.set({ json: passedJson }, function () {
       chrome.storage.local.get(["json"], function (result) {
@@ -27,12 +28,6 @@ async function getRandomArtwork() {
     console.log("Json Retrieved:");
     console.log(result);
     let randInt = Math.floor(Math.random() * result["json"]["data"].length);
-    // console.log("Random artwork:");
-    // console.log(result["json"]["data"][randInt]);
-    // let test = result["json"]["data"][randInt];
-    // console.log(test);
     return result["json"]["data"][randInt];
   });
 }
-let test = getRandomArtwork();
-console.log(test);
