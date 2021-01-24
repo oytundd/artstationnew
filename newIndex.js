cbdceebe4d7005d6ced4a53ed337441409f0ba17
oytundd <oytundd@gmail.com>
2021-01-24 13:50:42 +0300

    Initial functions

--- newIndex.js
+++ newIndex.js
@@ -2,26 +2,37 @@
 
 let userUrl = "https://www.artstation.com/users/lemonhunter/likes.json";
 let userName = "lemonhunter";
-let userJson;
-let randomArtwork;
-async function fetchJson(url) {
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  } else {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
-  }
-}
-function updateJson(passedJson, userUrl) {
-  passedJson = fetchJson(userUrl).then((passedJson) => {
-    chrome.storage.local.set({ json: passedJson }, function () {
-      chrome.storage.local.get(["json"], function (result) {
-        console.log("Json Stored:");
-        console.log(result);
-      });
+function getStorage(key) {
+  //tested
+  return new Promise(function (resolve, reject) {
+    chrome.storage.local.get(["key"], function (result) {
+      console.log("v Result received from storage v");
+      console.log(result.key);
+      resolve(result);
     });
   });
+}
+function setStorage(key, value) {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.local.set({ key: value }, function () {
+      console.log("v Storage set v");
+      console.log(key, value);
+      resolve();
+    });
+  });
+}
+async function fetchJson(url) {
+  const RESPONSE = await fetch(url);
+  if (RESPONSE.ok) {
+    return RESPONSE.json();
+  } else {
+    const MESSAGE = "An error has occured:" + response.status;
+    throw MESSAGE;
+  }
+}
+async function updateJson(url) {
+  const USERJSON = await fetchJson(url);
+  await setStorage("json", USERJSON);
 }
 async function getRandomArtwork() {
   chrome.storage.local.get(["json"], function (result) {
